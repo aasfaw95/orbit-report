1f7cbfa457a53cb4707fd9eac8a1076f23109f3c
Aby <abyssiniaasfaw@gmail.com>
2020-08-05 14:56:54 -0500

    added seperate columns to chart

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,6 +4,12 @@ orbitType: string;
 type: string;
 operational: boolean;
 launchDate: string;
-constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {}
+constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    this.name = name;
+    this.orbitType = orbitType;
+    this.type = type;
+    this.operational = operational;
+    this.launchDate = launchDate;
+} 
 }
 
